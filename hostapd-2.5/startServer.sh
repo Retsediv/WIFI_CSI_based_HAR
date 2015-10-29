173711e84cd7189d31ce9e70e8a9a77548238096
Yaxiong Xie <xieyaxiongfly@gmail.com>
2015-10-29 14:23:25 +0800

    10-29-2015

--- hostapd-2.5/startServer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo /bin/bash -c "
-restart isc-dhcp-server
-echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING -s 10.10.0.0/16 -o ppp0 -j MASQUERADE
-./hostapd  ./hostapd.conf -B
-"
